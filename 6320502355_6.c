98d23097642be591fa17e1b38f75e83fbe8f4c2d
kulwatee <kulwatee.j@ku.th>
2021-03-18 16:00:27 +0700

    ee

--- 6320502355_6.c
+++ 6320502355_6.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,i,j;
     scanf("%d",&n);
+    fflush(stdin);
     char nn[n][n],x[250];
     gets(x);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+            nn[i][j]='.';
+    }
+
+
 
 
 }
