eda84ace979085f68350b98eee9372629137e443
kulwatee <kulwatee.j@ku.th>
2021-03-18 15:55:08 +0700

    hh

--- 6320502355_4.c
+++ 6320502355_4.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,max,min,q;
+    int n,i,j,max,min,q,w;
     scanf("%d",&n);
     int a[n][2],b[n];
+    for(w=0;w<n;w++)
+        b[w]=0;
     for(i=0; i<n; i++)
     {
         for(j=0; j<2; j++)
@@ -14,7 +16,7 @@ int main()
     }
     max=1;
     min=1;
-    for(q=0; q<n; q++)
+    for(q=2; q<n+1; q++)
     {
         if(b[q]>b[max])
             max=q;
