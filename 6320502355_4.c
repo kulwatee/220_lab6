ca31c6a9092eaaaa19ffc93819937b51c2f19162
kulwatee <kulwatee.j@ku.th>
2021-03-18 15:50:19 +0700

    qq

--- 6320502355_4.c
+++ 6320502355_4.c
@@ -4,15 +4,17 @@ int main()
     int n,i,j,max,min,q;
     scanf("%d",&n);
     int a[n][2],b[n];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
-        for(j=0;j<2;j++)
+        for(j=0; j<2; j++)
+        {
             scanf("%d",&a[i][j]);
-        b[a[i][j]]++;
+            b[a[i][j]]++;
+        }
     }
     max=1;
     min=1;
-    for(q=0;q<n;q++)
+    for(q=0; q<n; q++)
     {
         if(b[q]>b[max])
             max=q;
