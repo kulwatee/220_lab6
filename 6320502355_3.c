b2a216dda3033c9a2497c8e7240fd0fe28afb6b3
kulwatee <kulwatee.j@ku.th>
2021-03-18 15:23:56 +0700

    ff

--- 6320502355_3.c
+++ 6320502355_3.c
@@ -1,13 +1,32 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,h;
+    int n,m,h,b,c,i,max;
     scanf("%d %d",&n,&m);
     signed long a[n];
     char t;
-    for(h=0;h<n;h++)
+    for(h=0; h<n; h++)
     {
         a[h]=0;
     }
+    for(i=0; i<m; i++)
+    {
+        scanf("%c %d %ld",&t,&b,&c);
+        if(t=='U')
+        {
+            a[b]=c;
+        }
+        else if(t=='P')
+        {
+            max=a[b];
+            while(b<c)
+            {
+                max=a[c];
+            }
 
+
+
+        }
+
+    }
 }
